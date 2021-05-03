5493a1419644f31d601420ba097e790d07e9ad00
philwoz <61164019+philwoz@users.noreply.github.com>
2021-05-03 23:41:49 +0100

    formatted game.js

--- game.js
+++ game.js
@@ -15,12 +15,12 @@ let currentScore = 0
 
 
 const changePlayer = (currentPlayer) => {
-    if(currentPlayer == 1 ){
+    if (currentPlayer == 1) {
         return 2
     } else if (currentPlayer == 2) {
         return 1
     }
-    
+
 }
 
 
@@ -42,7 +42,7 @@ start.addEventListener("click", () => {
     totalTwo.innerHTML = scoreTwo
     roll.style.display = ""
     hold.style.display = ""
-    
+
 })
 
 
@@ -51,72 +51,72 @@ let totalScore = 0
 
 roll.addEventListener("click", () => {
     let dice = randomNum()
-    image.src=`./assets/dice${dice}.png`;
-    if(player == 1){
-        totalScore = scoreOne 
-        
-    } else if (player == 2){
-        totalScore = scoreTwo 
+    image.src = `./assets/dice${dice}.png`;
+    if (player == 1) {
+        totalScore = scoreOne
+
+    } else if (player == 2) {
+        totalScore = scoreTwo
     }
-    currentScore += dice 
+    currentScore += dice
     totalScore += currentScore
-    
-    
-    if(dice == 1){
+
+
+    if (dice == 1) {
         currentScore = 0
         message.innerHTML = `Player ${player} current score lost! <br> next player to roll!</br>`
-        if(player == 1 ){
+        if (player == 1) {
             player = 2
             currentPlayer.innerHTML = "Player Two"
             currentPlayer.style.backgroundColor = "#F15BB5"
             currentOne.innerHTML = currentScore
-    
+
         } else if (player == 2) {
             currentPlayer.innerHTML = "Player One"
             currentPlayer.style.backgroundColor = "#9B5DE5"
             currentTwo.innerHTML = currentScore
             player = 1
-            
+
         }
-        
-       
-    } else if ((totalScore) >= 20){
+
+
+    } else if ((totalScore) >= 20) {
         roll.style.display = "none"
         hold.style.display = "none"
         message.innerHTML = "you win!"
-        if(player == 1){
+        if (player == 1) {
             currentOne.innerHTML = currentScore
             totalOne.innerHTML = totalScore
-        } else if (player == 2){
+        } else if (player == 2) {
             currentTwo.innerHTML = currentScore
             totalTwo.innerHTML = totalScore
         }
         start.innerHTML = "Start new Game"
-    } else if(totalScore < 20){
-        if(player == 1){
-           
+    } else if (totalScore < 20) {
+        if (player == 1) {
+
             message.innerHTML = `Hold or Roll again?`
             currentOne.innerHTML = currentScore
-            
-        } else if (player == 2){
-            
+
+        } else if (player == 2) {
+
             message.innerHTML = `Hold or Roll again?`
             currentTwo.innerHTML = currentScore
-            
+
         }
 
-}
+    }
 
 
-    
+
 })
 
 
 
 hold.addEventListener("click", () => {
-    
+
     message.innerHTML = `Your Turn To Roll!`
-    if(player == 1){
+    if (player == 1) {
         scoreOne += currentScore
         currentScore = 0
         totalOne.innerHTML = scoreOne
@@ -124,8 +124,8 @@ hold.addEventListener("click", () => {
         currentPlayer.innerHTML = "Player Two"
         currentPlayer.style.backgroundColor = "#F15BB5"
         currentOne.innerHTML = currentScore
-        
-    } else if (player == 2){
+
+    } else if (player == 2) {
         scoreTwo += currentScore
         currentScore = 0
         totalTwo.innerHTML = scoreTwo
@@ -133,8 +133,8 @@ hold.addEventListener("click", () => {
         currentPlayer.innerHTML = "Player One"
         currentPlayer.style.backgroundColor = "#9B5DE5"
         currentTwo.innerHTML = currentScore
-        
+
     }
-    
-    
+
+
 })
